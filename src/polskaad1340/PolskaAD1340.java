14a42db039653f68dd6bd58ccbd33e678c46ef51
wyzellak <b4k4squared@gmail.com>
2013-05-08 15:07:43 +0200

    Szablon głównej klasy

---
+++ src/polskaad1340/PolskaAD1340.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polskaad1340;
+
+/**
+ *
+ * @author Kuba
+ */
+public class PolskaAD1340 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
