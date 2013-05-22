befd3059eb5731f270e30b35b7b7e6cfc3f14bc1
Kamil Popinski <popinski.kamil@gmail.com>
2013-05-22 09:36:58 +0200

    implementacja statystyk drwala

---
+++ src/statistics/WoodmanStatistics.java
@@ -0,0 +1,88 @@
+
+package statistics;
+
+/**
+ *
+ * @author Kamil
+ */
+public class WoodmanStatistics implements WoodmanStatistics_Interface{
+    
+    /**
+     * ilość sciętych drzwe
+     * @var int
+     */
+    protected int _numberOfShearedWoods;
+    
+    /**
+     * Przychód
+     * @var int
+     */
+    protected int _income;
+    
+    /**
+     * Zysk
+     * @var int
+     */
+    protected int _profit;
+    
+    /**
+     * Setter dla ilości ściętych drzew.
+     * @param int numberOfShearedWoods
+     * @return WoodmanStatistics_Interface
+     */
+    @Override
+    public WoodmanStatistics_Interface setNumberOfShearedWoods( int numberOfShearedWoods){
+        this._numberOfShearedWoods=numberOfShearedWoods;
+        return this;
+    }
+    
+    /**
+     * Getter dla ilości ściętych drzew.
+     * @return 
+     */
+    @Override
+    public int getNumberOfShearedWoods(){
+        return this._numberOfShearedWoods;
+    }
+    
+    /**
+     * Setter dla całkowitego zysku.
+     * @param int profit
+     * @return WoodmanStatistics_Interface
+     */
+    @Override
+    public WoodmanStatistics_Interface setProfit(int profit){
+        this._profit=profit;
+        return this;
+    }
+    
+    /**
+     * Getter dla całkowtego zysku.
+     * @return int
+     */
+    @Override
+    public int getProfit(){
+        return this._profit;
+    }
+    
+    /**
+     * Setter dla całkowitego przychodu.
+     * @param int income
+     * @return WoodmanStatistics_Interface
+     */
+    @Override
+    public WoodmanStatistics_Interface setIncome(int income){
+        this._income=income;
+        return this;
+    }
+    
+    /**
+     * Getter dla całkowitego przychodu.
+     * @return int
+     */
+    @Override
+    public int getIncome(){
+        return this._income;
+    }
+    
+}
