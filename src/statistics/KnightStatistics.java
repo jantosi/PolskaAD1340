baa4a159c2bb66111a391c93f3bc827d4d3af59d
Kamil Popinski <popinski.kamil@gmail.com>
2013-05-22 09:49:58 +0200

    implementacja statystyk rycerza

--- src/statistics/KnightStatistics.java
+++ src/statistics/KnightStatistics.java
@@ -5,6 +5,58 @@ package statistics;
  *
  * @author Kamil
  */
-public class KnightStatistics {
+public class KnightStatistics implements KnightStatistics_Interface{
+    
+    /**
+     * liczba zabitych smoków
+     * @var int
+     */
+    protected int _numberOfKilledDragons;
+    
+    /** 
+     * liczba wizyt w grodach
+     * @var int
+     */
+    protected int _numberOfVisitsInCities;
+    
+    /**
+     * Setter dla liczby zabitych smoków.
+     * @param int numberOfKilledDragons
+     * @return KnightStatistics_Interface
+     */
+    @Override
+    public KnightStatistics setNumberOfKilledDragons(int numberOfKilledDragons){
+        this._numberOfKilledDragons=numberOfKilledDragons;
+        return this;
+    }
+    
+    /**
+     * Getter dla liczby zabitych smoków.
+     * @return int
+     */
+    @Override
+    public int getNumberOfKilledDragons(){
+        return this._numberOfKilledDragons;
+    }
+    
+    /**
+     * Setter dla liczby wizyt w grodach.
+     * @param int numberOfVisitsInCities
+     * @return KnightStatistics_Interface
+     */
+    @Override
+    public KnightStatistics setNumberOfVisitsInCities(int numberOfVisitsInCities){
+        this._numberOfVisitsInCities=numberOfVisitsInCities;
+        return this;
+    }
+    
+    /**
+     * Getter dla liczby wizyt w grodach.
+     * @return int
+     */
+    @Override
+    public int getNumberOfVisitsInCities(){
+        return this._numberOfVisitsInCities;
+    }
     
 }
